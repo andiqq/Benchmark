d1616e93b4affe27ed4696182b7bd5b4897c75eb
Dr. Andreas Plagens <andreas@plagens.eu>
2024-08-10 17:20:52 +0200

    initial

--- Package.swift
+++ Package.swift
@@ -5,10 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "Benchmark",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .executable(name: "app", targets: ["App"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.52.2")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Benchmark"),
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+            ],
+            path: "Sources"),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,23 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Vapor
 
-print("Hello, world!")
+var env = try Environment.detect()
+try LoggingSystem.bootstrap(from: &env)
+
+let app = Application(env)
+defer { app.shutdown() }
+
+app.http.server.configuration.serverName = "Vapor"
+app.http.server.configuration.supportPipelining = false
+
+app.logger.notice("💧 VAPOR")
+app.logger.notice("System.coreCount: \(System.coreCount)")
+
+app.get("plaintext") { req async in
+    "Hello, world!"
+}
+
+app.get("json") { req async in
+    ["message": "Hello, world!"]
+}
+
+try app.run()
